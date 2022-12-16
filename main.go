5a5f73b6fc90d96c27c9560a3d19b468d447e77a
Nick Webb <nwebb@vmware.com>
2022-12-15 16:43:23 -0800

    Change

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var addr = flag.String("addr", ":8080", "addr to bind to")
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 
-	fmt.Fprintf(w, "test2 I'm glad I spend Fridays with ALM!\n")
+	fmt.Fprintf(w, "Testing with ootb poc\n")
 }
 
 func main() {
